cfa5d1bae143686c875614885df8ae71605b2fe3
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-12-05 23:57:47 +0100

    Add read property toggle and erase book function

--- script.js
+++ script.js
@@ -5,17 +5,30 @@ const btnDelete = document.getElementById('delete');
 
 const library = [
     {
-        'title' : 'The demolished man',
-        'author': 'Alfred Bester',
-        'pages' : 300,
+        'title' : 'A Tale of Two Cities',
+        'author': 'Charles Dickens',
+        'year' : 1859,
+        'read'  : 'false'
+    }, 
+    {
+        'title' : 'To Kill a Mockingbird',
+        'author': 'Harper Lee',
+        'year' : 1960,
+        'read'  : 'false'
+    }, 
+    {
+        'title' : 'The Name of the Rose',
+        'author': 'Umberto Eco',
+        'year' : 1980,
         'read'  : 'false'
     }
 ];
 
-function book(title, author, pages, read) {
+function book(title, author, year, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.year = year;
+    this.read = read;
 }
 
 function displayBooks() {
@@ -23,7 +36,7 @@ function displayBooks() {
         const book = document.createElement('div');
         const cover = document.createElement('div');
         const author = document.createElement('div');
-        const pages = document.createElement('div');
+        const year = document.createElement('div');
         const status = document.createElement('div');
         const read = document.createElement('div');
         const readIcon = document.createElement('span');
@@ -31,10 +44,11 @@ function displayBooks() {
         const trashIcon = document.createElement('span');
         const coverText = document.createElement('h2');
 
+        book.setAttribute('data-index', library.indexOf(item));
         book.classList.add('book-card');
         cover.classList.add('cover');
         author.classList.add('author');
-        pages.classList.add('pages');
+        year.classList.add('year');
         status.classList.add('status');
         read.classList.add('read');
         readIcon.classList.add('material-symbols-outlined');
@@ -43,24 +57,35 @@ function displayBooks() {
         readIcon.innerText = 'visibility_off';
         trashIcon.innerText = 'delete';
         coverText.innerText = item.title;
-        author.innerText = item.author;
-        pages.innerText = item.pages;
+        cover.style.backgroundColor = randomHsl();
+        author.innerText = `By: ${item.author}`;
+        year.innerText = `Year: ${item.year}`;
 
         read.addEventListener('click', (e) => {
             if (readIcon.innerText == 'visibility_off') {
-                toggleRead (e.target);
+                e.target.classList.add('toggleOn');
                 readIcon.innerText = 'visibility';
+                item.read = 'true';
             }
             else {
                 readIcon.innerText = 'visibility_off';
-                toggleRead (e.target);
+                e.target.classList.remove('toggleOn');
+                item.read = 'false';
             }
         });
+
+        trash.addEventListener('click', (e) => {
+            book.remove();
+        });
+
+        function randomHsl() {
+            return 'hsl(' + (Math.random() * 360) + ', 50%, 42%)';
+        }
         
         cover.appendChild(coverText);
         book.appendChild(cover);
         book.appendChild(author);
-        book.appendChild(pages);
+        book.appendChild(year);
         book.appendChild(status);
         read.appendChild(readIcon);
         trash.appendChild(trashIcon);
@@ -68,10 +93,6 @@ function displayBooks() {
         status.appendChild(trash);
         catalog.appendChild(book);
     });
-};
-
-function toggleRead (element) {
-    element.classList.toggle('toggleOn');
 };
 
 displayBooks();
